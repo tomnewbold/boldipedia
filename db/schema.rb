2c5ba3b6564d6e0519d86335c330e17e2249a210
Tommy Newbold <tommynewbold@googlemail.com>
2015-10-19 22:11:02 +0100

    Added ability to crud wikis

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151013212139) do
+ActiveRecord::Schema.define(version: 20151019182459) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -26,9 +26,25 @@ ActiveRecord::Schema.define(version: 20151013212139) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.string   "unconfirmed_email"
   end
 
+  add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "wikis", force: :cascade do |t|
+    t.string   "title"
+    t.text     "body"
+    t.boolean  "private"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "wikis", ["user_id"], name: "index_wikis_on_user_id"
 
 end
