e02e18f483dce41a6399b5791c8431dec3a48902
Tommy Newbold <tommynewbold@googlemail.com>
2015-10-25 21:43:14 +0000

    Added authorization through pundit

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,24 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :confirmable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :wikis
+
+  after_initialize do 
+    self.role ||= "standard"
+  end
+
+  def admin?
+    role == "admin"
+  end
+
+   def premium?
+    role == "premium"
+  end
+
+   def standard?
+    role == "standard"
+  end
+
+
 end
