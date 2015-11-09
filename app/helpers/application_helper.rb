1e0c3b69696fb35f183496f437f42fd5e205ca7a
Tommy Newbold <tommynewbold@googlemail.com>
2015-11-09 21:32:56 +0000

    Wikis now support markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,11 @@ module ApplicationHelper
   def my_name
     "Tom Newbold"
   end
+
+   def markdown(markdown)
+    renderer = Redcarpet::Render::HTML.new
+    extensions = {fenced_code_blocks: true}
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render markdown).html_safe
+  end
 end
